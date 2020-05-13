e980dce75efcab3aea34ae89476c81be5e8a0007
xxlxep <xxlxep@gmail.com>
2020-05-13 13:04:35 +0300

    AnswerItem

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,22 +1,17 @@
 import React from "react";
 import classes from "./ActiveQuiz.module.css";
+import AnswersList from "./AnswersList/AnswersList";
 
 const ActiveQuiz = props => {
   return (
     <div className={classes.ActiveQuiz}>
       <p className={classes.Question}>
         <span>
-          <strong>2.</strong>&nbsp;
-          Как Дела?
+          <strong>2.</strong>&nbsp; Как Дела?
         </span>
         <small>4 из 12</small>
       </p>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </ul>
+      <AnswersList answers={props.answers} />
     </div>
   );
 };
