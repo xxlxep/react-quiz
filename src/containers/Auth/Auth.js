f0441d3ca07821e5b48580cd947824071b3488c8
xxlxep <xxlxep@gmail.com>
2020-06-09 12:18:46 +0300

    Auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -10,6 +10,7 @@ function validateEmail(email) {
 
 export default class Auth extends Component {
   state = {
+    isFormValid: false,
     formControls: {
       email: {
         value: "",
@@ -69,7 +70,11 @@ export default class Auth extends Component {
     control.touched = true;
     control.valid = this.validateControl(control.value, control.validation);
     formControls[controlName] = control;
-    this.setState({ formControls });
+    let isFormValid = true;
+    Object.keys(formControls).forEach((name) => {
+      isFormValid = formControls[name].valid && isFormValid;
+    });
+    this.setState({ formControls, isFormValid });
   };
 
   renderInputs() {
@@ -99,11 +104,19 @@ export default class Auth extends Component {
           <form className={classes.AuthForm} onSubmit={this.submitHandler}>
             {this.renderInputs()}
 
-            <Button type="success" onClick={this.loginHandler}>
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+              disabled={!this.state.isFormValid}
+            >
               {" "}
               Войти
             </Button>
-            <Button type="primary" onClick={this.registerHandler}>
+            <Button
+              type="primary"
+              onClick={this.registerHandler}
+              disabled={!this.state.isFormValid}
+            >
               {" "}
               Зарегистрироваться
             </Button>
